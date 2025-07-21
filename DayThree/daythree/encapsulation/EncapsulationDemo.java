83a4cc2b462e681448eac362a743de3decea2c02
Rajlaxmi_Lagad <152899509+rajlaxmi05-code@users.noreply.github.com>
2025-07-21 22:47:33 +0530

    Add files via upload

---
+++ DayThree/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,21 @@
+package DayThree.daythree.encapsulation;
+
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+
+		
+		//object creation
+		
+		OopsConceptDemo obj = new OopsConceptDemo();
+		
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(21);
+		
+		System.out.println(obj);
+		
+	}
+
+}

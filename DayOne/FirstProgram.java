0103ad11160a975c175fc11f5ecc5cfd0b9eb5e5
Rajlaxmi_Lagad <152899509+rajlaxmi05-code@users.noreply.github.com>
2025-07-21 22:25:59 +0530

    Add files via upload

---
+++ DayOne/FirstProgram.java
@@ -0,0 +1,20 @@
+
+
+public class FirstProgram {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World"); //to print the value
+		
+		System.out.println(34);
+		
+		System.out.print(10+20);
+		System.out.print(10+30);
+		
+		//Base b1 = new Base();
+		
+	}
+
+}
+
+

10bb6b0365f61f3d263be4fa471e2a5c728c2e7e
Rajlaxmi_Lagad <152899509+rajlaxmi05-code@users.noreply.github.com>
2025-07-21 23:06:42 +0530

    Add files via upload

---
+++ DayFour/defaultmodifie/ModifierDemo.java
@@ -0,0 +1,17 @@
+package DayFour.defaultmodifie;
+
+
+
+import com.tnsif.dayfour.modifiers.Base;
+
+public class ModifierDemo extends Base {
+
+	public static void main(String[] args) {
+
+		Base b1 = new Base();
+		// b1.methodDefault(); will show error
+	//	b1.methodProtected();
+		
+	}
+
+}
